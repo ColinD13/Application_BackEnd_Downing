0bb587a1924e20fd22da2bc2762dc66181d5698e
ColinD13 <downingcolin13@gmail.com>
2026-03-18 20:12:53 -0400

    Adding cors for the error I was getting
    Adding CORS to make sure my Frontend can use my API

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 
 const { Pool } = require('pg');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const pool = new Pool({
     host:process.env.DB_HOST,
