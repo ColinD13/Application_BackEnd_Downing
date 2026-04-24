e4231c95374a81636d85b8a725480ba1590f8825
ColinD13 <downingcolin13@gmail.com>
2026-04-23 22:41:06 -0400

    Editing the user put
    Edit the user put

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,69 +1,77 @@
-const pool = require('../dbs/db');
+const pool = require("../dbs/db");
 
 const getUsers = async (req, res) => {
-    try{
-        const result = await pool.query('select * from public.users');
-        res.json(result.rows);
-    }catch (err){
-        console.error(err);
-        res.status(500).json({error : "Users retrieval query fail"});
-    }
-}
+  try {
+    const result = await pool.query("select * from public.users");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Users retrieval query fail" });
+  }
+};
 
 const getUserById = async (req, res) => {
-    try{
-        const user_id = req.params.id;
-        const result = await pool.query('select * from public.users where user_id = $1', [user_id]);
-        res.json(result.rows);
-    }catch (err){
-        console.error(err);
-        res.status(500).json({error : "User retrieval query fail"});
-    }
-}
+  try {
+    const user_id = req.params.id;
+    const result = await pool.query(
+      "select * from public.users where user_id = $1",
+      [user_id],
+    );
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "User retrieval query fail" });
+  }
+};
 
 const postUser = async (req, res) => {
-    try{
-        console.log("POST HIT");
-        const { name } = req.body;
+  try {
+    console.log("POST HIT");
+    const { name } = req.body;
 
-        const result = await pool.query("insert into public.users (name) values ($1)", [name]);
+    const result = await pool.query(
+      "insert into public.users (name) values ($1)",
+      [name],
+    );
 
-        res.status(201).json({response: "Added the information you requested"})
-    }
-    catch(err){
-        console.log(err);
-        res.status(500).json({error:"Server error"});
-    }
-}
+    res.status(201).json({ response: "Added the information you requested" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
 
-const putUser = async (req,res) => {
-    try{
-        //get the params sent in the body to post
-        const { name, user_id} = req.body;
+const putUser = async (req, res) => {
+  try {
+    //get the params sent in the body to post
+    const { name, user_id } = req.body;
 
-        const result = await pool.query("update public.users set name = ($2) where user_id = ($1)", [user_id, name]);
+    const result = await pool.query(
+      `UPDATE public.users SET name = $2 WHERE user_id = $1 RETURNING *`,
+      [parseInt(user_id), name],
+    );
+    res.status(201).json({ response: "Updated the information you requested" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
 
-        res.status(201).json({response: "Updated the information you requested"});
-    }
-    catch(err){
-        console.error(err);
-        res.status(500).json({error:"Server error"});
-    }
-}
+const deleteUser = async (req, res) => {
+  try {
+    //get the params sent in the body to post
+    const { user_id } = req.body;
 
-const deleteUser = async (req,res) => {
-    try{
-        //get the params sent in the body to post
-        const { user_id } = req.body;
+    const result = await pool.query(
+      "delete from public.users where user_id = $1",
+      [user_id],
+    );
 
-        const result = await pool.query("delete from public.users where user_id = $1", [user_id]);
+    res.status(201).json({ response: "Deleted the information you requested" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
 
-        res.status(201).json({response: "Deleted the information you requested"});
-    }
-    catch(err){
-        console.error(err);
-        res.status(500).json({error:"Server error"});
-    }
-}
-
-module.exports = { getUsers, getUserById, postUser, putUser, deleteUser};
+module.exports = { getUsers, getUserById, postUser, putUser, deleteUser };
